284796c9ecb02d38605b6761302364f6141a4470
wlisses <wlissesfernando285@gmail.com>
2022-12-12 18:32:36 -0300

    concluído exercicío para adicionar Nelson Muntz

---
+++ first_exercise/src/addCharacter.js
@@ -0,0 +1,16 @@
+const fs = require('fs').promises;
+const path = require('path');
+const WAY_DATA_JSON = '../util/simpsonsFamily.json';
+
+const newCharacter = async (newValue) => {
+
+  const getJson = await fs.readFile(path.resolve(__dirname, WAY_DATA_JSON));
+  const parsing = JSON.parse(getJson);
+  const allValues = [...parsing, newValue];
+  await fs.writeFile(path.resolve(__dirname, WAY_DATA_JSON), JSON.stringify(allValues))
+};
+
+newCharacter({
+  id: "?",
+  name: "??????????"
+});
